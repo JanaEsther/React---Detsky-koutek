599a1c3ff4e894eaecd923919cdc5ab89bfa9bfa
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-11-16 17:15:47 +0100

    centerDetail

--- src/pages/CenterDetail/index.jsx
+++ src/pages/CenterDetail/index.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { useParams, Link } from 'react-router-dom';
 import './style.css'
 
-import React from 'react'
+
 
 const CenterDetail = () => {
-  return <div>
-    
-  </div>;
-};
+  const { CenterId } = useParams();
+  const center =movies.find((oneCenter)=>{
+return oneCenter.id === parseInt(CenterId)
+  })
+
+  const {name, address, capacity, open, info} = center
+
+  return (
+    <section className="one-center-more">
+      <p>{name}</p>
+      <p>Adresa:{address}</p>
+      <p>Kapacita:{capacity}</p>
+      <p className="open">Otevírací hodiny:{open}</p>
+      <p>Informace: {info}</p>
+      <Link to="/centers">Zpět na všechny pobočky</Link>
+    </section>
+  );
+}
 
 export default CenterDetail;
--- src/pages/CentersPage/index.jsx
+++ src/pages/CentersPage/index.jsx
@@ -1,43 +1,45 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import {Link, Outlet} from 'react-router-dom'
 
 export const CentersPage = () => {
   const [centers, setCenters] = useState([]);
-  
+
   useEffect(() => {
     const fetchCenters = async () => {
-     
-        const response = await fetch('http://localhost:4000/api/centers');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const json = await response.json();
-        setCenters(json.data);
-        console.log(data)
-  
+      const response = await fetch('http://localhost:4000/api/centers');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setCenters(data);
+      console.log(data);
     };
 
     fetchCenters();
   }, []);
 
-  if (centers.length === null){
-    return <p>Loading ...</p>
+  if (centers.length === null) {
+    return <p>Loading ...</p>;
   }
 
   return (
-      <div className="all-centers">
-        <h1>Pobočky</h1>
-        {centers.map((center) => {
-          const { id, name, address} = center;
-
-          return (
-            <div className="one-center" key={id}>
-              <h2>{name}</h2>
-              <p>{address}</p>
-            </div>
-          );
-        })}
-      </div>
-   
+    <div className="all-centers">
+      {' '}
+      <h1>Pobočky</h1>{' '}
+      <ul>
+        {' '}
+        {centers.map((center) => (
+          <li key={center.id}>
+            {' '}
+            <Link to={`/pobocky/${center.id}`}>
+              {' '}
+              <h2>{center.name}</h2> <p>{center.address}</p>{' '}
+            </Link>{' '}
+          </li>
+        ))}{' '}
+      </ul>{' '}
+      <Outlet />{' '}
+    </div>
   );
-}
+};
